b7bc284ba598897d48cd565d26b2568454eb5bdf
Toqeeb Ahmed <128019854+toqeebahmed@users.noreply.github.com>
2023-03-16 20:52:22 +0500

    Create userinput.cpp

---
+++ userinput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{ 
+int a ;
+int b ;
+int c ;
+cout<<"please enter first value" ;
+cin >> a ;
+cout<< "please enter second value" ;
+cin >> b ;
+c = a+b ;
+cout <<"The result is" ;
+cin>> c ;
+}
