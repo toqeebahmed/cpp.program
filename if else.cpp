a2272406f96e9801a7098b6cf3efb76cbdc69be7
Toqeeb Ahmed <128019854+toqeebahmed@users.noreply.github.com>
2023-03-16 21:06:07 +0500

    Create if else.cpp

---
+++ if else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{ 
+ int a = 10;
+ int b = 13;
+ if (a>=b)
+ { 
+ cout <<"a is greater than are equal to b";
+ 
+ }
+ else 
+ { 
+ cout <<"a is not greater than are equal to b";
+ 
+ }
+}
