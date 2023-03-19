6772134cfbe3623119058e16dde22cad72c8bb72
Toqeeb Ahmed <128019854+toqeebahmed@users.noreply.github.com>
2023-03-19 19:15:14 +0500

    Create parameter function.cpp

---
+++ parameter function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void Add(int, int);
+int main()
+{ 
+ int valueOne, valueTwo;
+ cout<<"please enter valueOne";
+ cin>>valueOne;
+ cout<<"please enter valueTwo";
+ cin>>valueTwo;
+}
+void Add (int a, int b)
+{ 
+ cout<<a+b;
+}
